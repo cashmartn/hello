456cc0aae73f5da304d0c4ca86d850c7f4876fb3
Cash Martin <cmartin39@unl.edu>
2020-08-26 06:34:38 +0000

    added hello.c and .gitignore

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Cash Martin
+ *         cmartin39@unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cash Martin!\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
